2d0af6dc29f9828a1db0ab04b15026b190274dd5
0jisu0 <kiju5757@naver.com>
2022-07-25 00:47:25 +0900

    img 추가

--- fishing.py
+++ fishing.py
@@ -1,3 +1,4 @@
+from curses.ascii import FF
 from selectors import EpollSelector
 import pyautogui 
 from time import sleep as t
@@ -8,27 +9,61 @@ sim = 0.83 #이미지 유사성
 #pyautogui.locateCenterOnScreen(confidence=0.9,  grayscale=True, region=(0,0, 300, 400))#유사성/흑백/x,y,가로, 세로
     # pyautogui.position()
 
-
+img = ''
 def ck():
-    capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
-    if capture == None:
-        t(1)
-    else:
-        break
+    while True:
+        capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+        if capture == None:
+            t(1)
+        else:
+            break
     center = pyautogui.center(capture)
     pyautogui.click(center)
+def find ():
+    while True:
+        capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+        if capture == None:
+            t(1)
+        else:
+            break
 
+def active():
+    def ck():
+        while True:
+            capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+            if capture == None:
+                t(1)
+            else:
+                break
+        center = pyautogui.center(capture)
+        pyautogui.click(center)
+    def find ():
+        while True:
+            capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+            if capture == None:
+                t(1)
+            else:
+                break
+
+    img = 'active_work'
+    ck()
+    img = 'm2'
+    ck()
+    img = 'echo'
+    while True:
+        capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+        if capture == None:
+            pyautogui.write('e')
+            t(1)
+        else:
+            break
+    center = pyautogui.center(capture)
+    pyautogui.click(center)
+    
 
 i = 1       
 while True:
-    img = ''
-    capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
-    if capture == None:
-        t(1)
-    else:
-        break
-    center = pyautogui.center(capture)
-    pyautogui.click(center)
+    ck()
 
 
 
