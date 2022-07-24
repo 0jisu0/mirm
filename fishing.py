55e1ac276a5548e53a2a2d645fd3c8086a004438
0jisu0 <kiju5757@naver.com>
2022-07-24 23:52:16 +0900

    click

--- fishing.py
+++ fishing.py
@@ -1,4 +1,38 @@
-import pyautogui
+from selectors import EpollSelector
+import pyautogui 
+from time import sleep as t
 
+ass = './img/'
+png = '.png'
+sim = 0.83 #이미지 유사성
+#pyautogui.locateCenterOnScreen(confidence=0.9,  grayscale=True, region=(0,0, 300, 400))#유사성/흑백/x,y,가로, 세로
+    # pyautogui.position()
+
+
+def ck():
+    capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+    if capture == None:
+        t(1)
+    else:
+        break
+    center = pyautogui.center(capture)
+    pyautogui.click(center)
+
+
+i = 1       
 while True:
-    
+    img = ''
+    capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+    if capture == None:
+        t(1)
+    else:
+        break
+    center = pyautogui.center(capture)
+    pyautogui.click(center)
+
+
+
+
+
+
+    i += 1
