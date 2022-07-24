468192a1552922c36c9a686efe395b124e44fe49
0jisu0 <kiju5757@naver.com>
2022-07-25 08:40:46 +0900

    active

--- fishing.py
+++ fishing.py
@@ -1,10 +1,10 @@
-from curses.ascii import FF
-from selectors import EpollSelector
+# from curses.ascii import FF
+# from selectors import EpollSelector
 import pyautogui 
 from time import sleep as t
 
-ass = './img/'
-png = '.png'
+ass = 'C:/Users/gram/Desktop/mirM/mirm/img/'
+png = '.PNG'
 sim = 0.83 #이미지 유사성
 #pyautogui.locateCenterOnScreen(confidence=0.9,  grayscale=True, region=(0,0, 300, 400))#유사성/흑백/x,y,가로, 세로
     # pyautogui.position()
@@ -14,60 +14,95 @@ def ck():
     while True:
         capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
         if capture == None:
+            print(str(capture)+': '+img)
             t(1)
         else:
             break
-    center = pyautogui.center(capture)
-    pyautogui.click(center)
+    pyautogui.click(capture)
+    print(img+': done')
 def find ():
     while True:
+        
         capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
         if capture == None:
+            print(str(capture)+': '+img)
             t(1)
         else:
             break
-
-def active():
+        print(img+': done')
+def active_2():
+    img = 'active_work'
     def ck():
         while True:
             capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
             if capture == None:
+                print(str(capture)+': '+img)
                 t(1)
             else:
-                break
-        center = pyautogui.center(capture)
-        pyautogui.click(center)
+                break    
+            pyautogui.click(capture)
+            print(img+': done')
     def find ():
         while True:
             capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
             if capture == None:
+                print(str(capture)+': '+img)     
                 t(1)
             else:
                 break
-
+            print(img+': done')
+        
     img = 'active_work'
     ck()
     img = 'm2'
     ck()
     img = 'echo'
     while True:
-        capture = pyautogui.locateCenterOnScreen(ass+img+png, confidence = sim)    
+        capture = pyautogui.locateOnScreen(ass+img+png, confidence = sim)    
         if capture == None:
-            pyautogui.write('e')
+            pyautogui.write('e',1)
+            print(str(capture)+': '+img)
             t(1)
         else:
             break
-    center = pyautogui.center(capture)
-    pyautogui.click(center)
-    
+    pyautogui.write('e')
 
-i = 1       
+# 모니터 해상도 가져오기
+width, height = pyautogui.size()
+print('width={0}, height={1}'.format(width, height))
+
+# i = 1       
+# while True:
+#     img = 'active_work'
+#     ck()
+#     img = 'm2'
+#     ck()
+#     img = 'echo'
+#     while True:
+#         capture = pyautogui.locateOnScreen(ass+img+png, confidence = sim)    
+#         if capture == None:
+#             pyautogui.write('e',1)
+#             print(str(capture)+': '+img)
+#             t(1)
+#         else:
+#             break
+#     pyautogui.write('e')
+
+#     i += 1
+
+
+
+img = 'active_work'
+ck()
+img = 'm2'
+ck()
+img = 'echo'
 while True:
-    ck()
-
-
-
-
-
-
-    i += 1
+    capture = pyautogui.locateOnScreen(ass+img+png, confidence = sim)    
+    if capture == None:
+        pyautogui.write('e',1)
+        print(str(capture)+': '+img)
+        t(1)
+    else:
+        break
+pyautogui.write('e')
