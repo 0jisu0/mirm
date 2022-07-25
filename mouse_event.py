0a47845dc02f5b79e5c9096e175bafc9a96a1224
0jisu0 <kiju5757@naver.com>
2022-07-25 09:03:24 +0900

    mouse

---
+++ mouse_event.py
@@ -0,0 +1,18 @@
+import mouse  
+
+import time
+
+ 
+
+click  = 0    # 몇 번째 클릭인지저장할 변수  
+
+ 
+
+while True:
+    if mouse.is_pressed("left"):             # 마우스 왼쪽 클릭이면
+        click += 1                             # 클릭 숫자 증가
+        print('Left-Clicked: ' + str(click))   # 메시지 출력
+        pos = mouse.get_position()       # 현재 마우스 포인터 좌표
+        print(pos)                                  
+        mouse.move(pos[0], pos[1]+20)  # 마우스 포인터 좌표를 아래로 이동
+    time.sleep(0.05)                          # 0.05초 대기 (중복 체크 예방)
